a3455e6a9024e31e4755a1c105af421fbd0edab1
2025103030021 <v.costa@estudante.ifro.edu.br>
2026-08-20 16:09:31 -0400

    okokkkkk

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -44,3 +44,32 @@ export async function buscarPorId(req, res) {
         res.status(500).json({erro: err.maessage});
     }
 }
+
+//funcao para atualizar um produto
+export async function atualizar(id, produto) {
+    try {
+        const [id] = req.params
+        const n = await service.atualizar(id, req.body)
+        if(n === 0) {
+            returnres.status(404).json({erro: 'Produto não atualizado'});
+        }
+        res.json({id, ...req.body});
+    } catch (err) {
+        
+        res.status(5000).json({err: message});
+    }
+}
+
+//funcao para deletar um produto
+export async function deletar(req, res) {
+try {
+    const {id} = req.params;
+    const n =await service.deletar(id);
+    if(n === 0) {
+        return res.status(404).json({erro: 'id invalido'})
+    }
+    res.status(204).send();
+} catch (err) {
+    res.satus(500).json({erro: err.message});
+}
+}
